e2fc02407ed222aa164dad676dfbc0c47270b265
MiaSimone <56347414+MiaSimone@users.noreply.github.com>
2020-08-28 19:56:28 +0200

    Tuesday2

---
+++ secondjpa/src/main/java/dbfacade/BookFacade.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbfacade;
+
+import entity.Book;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author miade
+ */
+public class BookFacade {
+    
+    private static EntityManagerFactory emf;
+    private static BookFacade instance;
+
+    public static void main(String[] args) {
+         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");      
+            BookFacade facade = BookFacade.getBookFacade(emf);
+            Book b1 = facade.addBook("J.K. Rowling");
+            Book b2 = facade.addBook("John Doe");
+            //Find book by ID
+            System.out.println("Book1: "+facade.findBook(b1.getId()).getAuthor());
+            System.out.println("Book2: "+facade.findBook(b2.getId()).getAuthor());
+            //Find all books
+            System.out.println("Number of books: "+facade.getAllBooks().size());
+
+    }
+    
+    private BookFacade(){
+    
+    }
+
+    // En metode til at hente et bog objekt ind ved at tjekke om objektet findes:
+    public static BookFacade getBookFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new BookFacade();
+        }
+        return instance;
+    }
+    
+    // En metode der tilføjer en bog til databasen:
+    public Book addBook(String author){
+        Book book = new Book(author);
+        EntityManager em = emf.createEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.persist(book);
+            em.getTransaction().commit();
+            return book;
+        }finally {
+            em.close();
+        }
+    }
+    
+    // En metode til at finde et bog objekt i databasen via dets id:
+    public Book findBook(int id){
+         EntityManager em = emf.createEntityManager();
+        try{
+            Book book = em.find(Book.class,id);
+            return book;
+        }finally {
+            em.close();
+        }
+    }
+    
+    // En metode der lister alle bog objekterne op fra databasen:
+    public List<Book> getAllBooks(){
+         EntityManager em = emf.createEntityManager();
+        try{
+            TypedQuery<Book> query = 
+                       em.createQuery("Select book from Book book",Book.class);
+            return query.getResultList();
+        }finally {
+            em.close();
+        }
+    }
+
+    
+    
+}
